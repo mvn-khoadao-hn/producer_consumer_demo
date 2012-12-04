0b96142b20bd8eafd8376b3e797b47546a383336
daoanhkhoa <khoada@lifetimetech.vn>
2012-12-04 10:46:10 +0700

    version 1

---
+++ src/demo/Main.java
@@ -0,0 +1,21 @@
+package demo;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Main {
+
+	@SuppressWarnings("rawtypes")
+	public static void main(String args[]) { 
+	     //Creating shared object
+	     BlockingQueue sharedQueue = new LinkedBlockingQueue();
+	 
+	     //Creating Producer and Consumer Thread
+	     Thread prodThread = new Thread(new Producer(sharedQueue));
+	     Thread consThread = new Thread(new Consumer(sharedQueue));
+
+	     //Starting producer and Consumer thread
+	     prodThread.start();
+	     consThread.start();
+	}
+}
